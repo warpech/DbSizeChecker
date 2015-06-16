61032c17d0e178ae97f52064154ab131398be95c
Marcin Warpechowski <warpech@gmail.com>
2015-06-16 07:09:01 +0200

    initial commit

---
+++ Generator.cs
@@ -0,0 +1,73 @@
+using System;
+using Starcounter;
+using System.IO;
+
+namespace Benchmark {
+    public class Generator {
+        Int32 countriesCount;
+
+        public Generator() {
+            var country = Db.SQL<Country>("SELECT c FROM Benchmark.Country c FETCH ?", 1).First;
+            if (country == null) {
+                CreateCountries();
+            }
+            countriesCount = (Int32)Db.SlowSQL<Int64>("SELECT COUNT(*) FROM Benchmark.Country c").First;
+        }
+
+        public void CreateCountries() {
+            string line;
+            string path = "C:\\PolyjuiceProjects\\warpech\\Benchmark\\countries.txt";
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            while ((line = file.ReadLine()) != null) {
+                Db.Transact(() => {
+                    new Country() {
+                        Name = line
+                    };
+                });
+            }
+            file.Close();
+        }
+
+        public Country GetRandomCountry() {
+            var r = new Random();
+            Int32 index = r.Next(0, countriesCount - 1);
+            var country = Db.SQL<Country>("SELECT c FROM Benchmark.Country c FETCH ? OFFSET ?", 1, index).First;
+            return country;
+        }
+
+        public Int64 CountObjects() {
+            UInt64 obj = Db.SQL<UInt64>("SELECT ObjectNo FROM Benchmark.WhatsAppUser c ORDER BY ObjectNo DESC FETCH ?", 1).First;
+            return Convert.ToInt64(obj);
+        }
+
+        public void CreateData() {
+            var max = 1000;
+            for (var i = 0; i < max; i++) {
+                Db.Transact(() => {
+                    new WhatsAppUser() {
+                        Country = GetRandomCountry(),
+                        CreatedAt = DateTime.Now,
+                        UserName = GetRandomString(8),
+                        PhoneNumber = GetRandomNumber()
+                    };
+                });
+            }
+        }
+
+        string GetRandomString(int len) {
+            string path = Path.GetRandomFileName();
+            path = path.Replace(".", ""); // Remove period.
+            return path.Substring(0, len);  // Return x character string
+        }
+
+        Int64 GetRandomNumber() {
+            Random random = new Random();
+            string r = "";
+            int i;
+            for (i = 1; i < 11; i++) {
+                r += random.Next(0, 9).ToString();
+            }
+            return Convert.ToInt64(r);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+using Starcounter;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Benchmark {
+
+    [Database]
+    public class Country {
+        public string Name;
+    }
+
+    [Database]
+    public class WhatsAppUser {
+        public DateTime CreatedAt;
+        public string UserName;
+        public Country Country;
+        public Int64 PhoneNumber;
+    }
+
+    public class Program {
+        Generator gen;
+
+        public static void Main() {
+            var p = new Program();
+            p.gen = new Generator();
+            p.ObservePerfmon();
+        }
+
+        public void ObservePerfmon() {
+            var process = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = "C:\\StarcounterProjects\\perfmon.exe",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    Arguments = "PERSONAL_DEFAULT"
+                }
+            };
+
+            process.OutputDataReceived += (sender, args) => ReadStats(args.Data);
+            process.ErrorDataReceived += (sender, args) => ReadStats(args.Data);
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            process.WaitForExit(); //you need this in order to flush the output buffer
+        }
+
+        char[] delimiterChars = { '=' };
+
+        Int64 startCountObjects = 0;
+        Int64 lastUsedPages = 0;
+        Int64 startUsedPages = 0;
+
+        void SendReport(string category, Int64 objectsCount, Int64 dbSize) {
+            Http.POST("http://localhost:8282/datapoint/" + category + "/" + objectsCount + "/" + dbSize, (string) null, null);
+        }
+
+        void ReadStats(string output) {
+            if (output.StartsWith(" storage_memory.used_pages")) {
+                Int64 usedPages = Convert.ToInt64(output.Split(delimiterChars)[1]);
+                if (usedPages != lastUsedPages) {
+                    lastUsedPages = usedPages;
+                    DbSession dbs = new DbSession();
+                    dbs.RunAsync(() => {
+                        Int64 countObjects = gen.CountObjects();
+                        if (startUsedPages == 0) {
+                            startUsedPages = usedPages;
+                            startCountObjects = countObjects;
+                        } else {
+                            Console.WriteLine(usedPages);
+                            SendReport("whatsapp", countObjects, usedPages);
+                            //SendReport("whatsapp2", countObjects - startCountObjects, usedPages - startUsedPages);
+                        }
+                        gen.CreateData();
+                    });
+                }
+            }
+            Console.WriteLine(output);
+
+        }
+
+    }
+}
